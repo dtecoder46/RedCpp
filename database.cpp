229c30a5ef823d4b0b316fc3ed80d4e44dc2b714
Daniel <dte.biotech46@gmail.com>
2025-04-29 22:36:49 -0400

    Created algorithm for RedC database

---
+++ database.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    /* Algorithm
+
+    1. Ask the user which data relation mode they want
+
+        Options: 
+            1-to-1
+            1-to-many
+
+        If the user's input is not one of the options, 
+        re-prompt the user until input is valid
+
+    2a. If the user chooses 1-to-1 mode, ask what they want to do with the data
+
+        Options: 
+            Add records to the database
+            Retrieve records from the database
+
+        If the user's input is not one of the options,
+        re-prompt the user until input is valid
+
+    2b. If the user chooses 1-to-many mode, ask what they want to do with the data  
+
+        Options:
+            Add records to the database
+            Retrieve one data field from a record
+            Retrieve all data fields from a record
+
+        If the user's input is not one of the options,
+        re-prompt the user until input is valid
+    
+    3a. ADD - Add 1-to-1 records to the database
+    
+    3b. FETCH - Retrieve 1-to-1 records from the database
+
+    3c. mADD - Add 1-to-many records to the database
+
+    3d. mFETCH - Retrieve one data field from a 1-to-many record
+
+    3e. mFETCH_ALL - Retrieve all data fields from a 1-to-many record
+    */
+}
