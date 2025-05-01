c8e8c0d9d8ee9e6a674d9ad24d968927b6eb0161
Daniel <dte.biotech46@gmail.com>
2025-05-01 12:26:10 -0400

    Simplified the algorithm, only SET and GET-like syntax will be used

--- database.cpp
+++ database.cpp
@@ -5,42 +5,10 @@ using namespace std;
 int main() {
     /* Algorithm
 
-    1. Ask the user which data relation mode they want
-
-        Options: 
-            1-to-1
-            1-to-many
-
-        If the user's input is not one of the options, 
-        re-prompt the user until input is valid
-
-    2a. If the user chooses 1-to-1 mode, ask what they want to do with the data
-
-        Options: 
-            Add records to the database
-            Retrieve records from the database
-
-        If the user's input is not one of the options,
-        re-prompt the user until input is valid
-
-    2b. If the user chooses 1-to-many mode, ask what they want to do with the data  
-
-        Options:
-            Add records to the database
-            Retrieve one data field from a record
-            Retrieve all data fields from a record
-
-        If the user's input is not one of the options,
-        re-prompt the user until input is valid
+    1. Ask for the user to input a phone model and a price
     
-    3a. ADD - Add 1-to-1 records to the database
-    
-    3b. FETCH - Retrieve 1-to-1 records from the database
+    2. Check that the phone model is a string and that the price is a number, prompt user until input is valid
 
-    3c. mADD - Add 1-to-many records to the database
-
-    3d. mFETCH - Retrieve one data field from a 1-to-many record
-
-    3e. mFETCH_ALL - Retrieve all data fields from a 1-to-many record
+    3. 
     */
 }
